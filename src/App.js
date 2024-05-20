743ca74d232eec1ac634ecd7efa25ba9fd2c5c51
GorraJS <gorrahabla1234@gmail.com>
2024-05-20 16:46:50 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [Peso, setPeso] = useState('');
+  const [Altura, setAltura] = useState('');
+
+  let IMC = Peso / Altura**2; 
+  
   return (
     <div className="App">
       <header className="App-header">
+        <label>Introduzca su peso</label>
+        <input type='number' value={Peso} onChange = {event => setPeso(event.target.value)}/>
+        
+        <label>Introduzca su altura </label>
+        <input type='number' value={Altura} onChange={ event => setAltura(event.target.value)}/>
+        
+        <p>Texto ingresado: { IMC } </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
